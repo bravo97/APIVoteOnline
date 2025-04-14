ecafa5a87c6fec25bb61cef0ddcb3c166371c57c
bravo97 <dobahung941997@gmail.com>
2025-04-14 15:28:56 +0700

    add home controller register

---
+++ VoteOnline.API/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VoteOnline.Domain.Models;
+using VoteOnline.Infratructure;
+
+namespace VoteOnline.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly SubAccountController _service;
+        public HomeController(SubAccountController service)
+        {
+            _service = service;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] SubAccountModel acc)
+        {
+            if (acc == null) { return BadRequest(new { Message = "Vui lòng nhập thông tin" }); }
+
+            
+            return Ok(acc);
+        }
+    }
+}
